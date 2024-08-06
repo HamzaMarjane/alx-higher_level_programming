f177e3a9e34ba5af14907741b78d062a907ce5ca
HamzaMarjane <hamza.marjanee@gmail.com>
2024-08-06 03:01:20 +0100

    Python

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,13 +1,2 @@
-#!/bin/bash
-
-# Check if database name is provided as argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <hbtn_0c_0>"
-    exit 1
-fi
-
-# Database name passed as argument
-hbtn_0c_0=$1
-
-# MySQL command to insert a new row into first_table
-mysql -u ojo_rachael -p -e "USE $hbtn_0c_0; INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
+-- Insert new row into a table.
+INSERT INTO first_table (`id`, `name`) VALUES(89, 'Holberton School');
